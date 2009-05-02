778c2bd56b3389aedd3d7b09c580aaa0eef3d9d3
ngu.kho <ngu.kho@0dda53ea-3736-11de-8fef-1f5ae417a2df>
2009-05-02 18:05:37 +0000

    COPYING: BSD License CODING: coding standards(draft), opinions needed! TEMPLATE.py: source template(draft), opinions needed!

---
+++ trunk/TEMPLATE.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# PyCi
+#
+# Copyright (c) 2009, The PyCi Project
+# Authors: Wu Ke <ngu.kho@gmail.com>
+#          Chen Xing <cxcxcxcx@gmail.com>
+# URL: <http://code.google.com/p/pyci>
+# For license information, see COPYING
+
+
+"""
+Write something. (-:
+"""
