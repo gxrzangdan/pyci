2b9a2666cae4adbe7c5d7b729b8f395107010a6f
ngu.kho <ngu.kho@0dda53ea-3736-11de-8fef-1f5ae417a2df>
2009-05-13 03:51:22 +0000

    Forward maximum matching Package interface

---
+++ trunk/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# PyCi
+#
+# Copyright (c) 2009, The PyCi Project
+# Authors: Wu Ke <ngu.kho@gmail.com>
+#          Chen Xing <cxcxcxcx@gmail.com>
+# URL: <http://code.google.com/p/pyci>
+# For license information, see COPYING
+
+
+"""PyCi -- a Chinese word segmentor library for Python
+"""
+
+__version__ = "0.0"
+
+import corpus
+import fmm
+import trie
---
+++ trunk/fmm.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+# PyCi
+#
+# Copyright (c) 2009, The PyCi Project
+# Authors: Wu Ke <ngu.kho@gmail.com>
+#          Chen Xing <cxcxcxcx@gmail.com>
+# URL: <http://code.google.com/p/pyci>
+# For license information, see COPYING
+
+
+"""Forward Maximum Matching word segmentor. Mainly used as a baseline
+segmentor.
+"""
+
+from copy import deepcopy
+
+from pyci.trie import Trie
+
+class FMMSeg(object):
+    """A forward maximum matching Chinese word segmentor.
+    """
+
+    def __init__(self, wordtrie=None, train=None):
+        """Construct a FMM Chinese word segmentor.
+
+        @type train: an iterable of words
+        @param train: training set
+        @type wordtrie: a trie of words
+        @param wordtrie: previously trained trie
+
+        If wordtrie is provided, it's deepcopied as the initial trie,
+        otherwise a new blank trie will be constructed.
+
+        If train is provided, it's appended into the trie above.
+        """
+        if wordtrie:
+            self._trie = deepcopy(wordtrie)
+        else:
+            self._trie = Trie()
+        if train:
+            self.add_words(train)
+
+    def add_words(self, train):
+        """Add train words into the trie.
+
+        @type train: an iterable of words
+        @param train: (possibly) new words
+        """
+        for word in train:
+            self._trie[word] = None
+
+    def seg(self, sent):
+        """Segment a sentence.
+
+        @type sent: unicode string
+        @param sent: the sentence to be segmented
+
+        @return: a list of segmented words
+        """
+        words = []
+        offset = 0
+        idx = self._trie.longest_prefix(sent, offset)
+        while offset < len(sent):
+            if idx == offset:
+                # the first character is not found in our trie, so
+                # treat it as a whole word
+                idx = offset + 1
+            words.append(sent[offset:idx])
+            offset = idx
+            idx = self._trie.longest_prefix(sent, offset)
+        return words
+
+
+def demo():
+    """Demo for FMM segmentor
+    """
+    from pyci.corpus import rmrb
+    words = [i for i in rmrb.words()]
+    seg = FMMSeg(train=words)
+    sent = u"马勒戈壁上的草泥马战胜了河蟹。"
+    print "/".join(seg.seg(sent))
+    seg.add_words([u"草泥马", u"马勒戈壁", u"河蟹"])
+    print "/".join(seg.seg(sent))
+
+
+if __name__ == "__main__":
+    demo()
