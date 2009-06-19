46fd9210c0e87d4b1d406c3a692994f2fbdaf885
ngu.kho <ngu.kho@0dda53ea-3736-11de-8fef-1f5ae417a2df>
2009-06-19 15:35:51 +0000

    TagSet class

---
+++ trunk/tagset/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# PyCi
+#
+# Copyright (c) 2009, The PyCi Project
+# Authors: Wu Ke <ngu.kho@gmail.com>
+#          Chen Xing <cxcxcxcx@gmail.com>
+# URL: <http://code.google.com/p/pyci>
+# For license information, see COPYING
+
+
+"""TagSet package.
+
+Contains several tag set used for tagging segmentation like CRF.
+"""
+
+__all__ = ["TagError", "TagSet"]
+
+from api import TagError, TagSet
