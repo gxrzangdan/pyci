5fde512326914fdf22cd44921dd64a4205a605ff
cxcxcxcx <cxcxcxcx@0dda53ea-3736-11de-8fef-1f5ae417a2df>
2009-06-21 11:13:21 +0000

    Change tag set template

--- trunk/tagset/template.py
+++ trunk/tagset/template.py
@@ -15,27 +15,40 @@ __all__ = ['head_tail_tagger', 'head_tail', 'head_tail_single_tagger', 'head_tai
 
 from api import *
 
-def head_tail_tagger(word):
-    res = []
-    if word:
-        res.append((word[0], 'H'))
-        for i in word[1:]:
-            res.append((i, 'T'))
-    return res
+class HeadTailTagSet(TagSet):
+    """A tag set distinguishes only whether the character is the head of the word.
+    """
 
-head_tail = TagSet(['H'], ['T'], head_tail_tagger)
+    def __init__(self):
+        def head_tail_tagger(word):
+            res = []
+            if word:
+                res.append((word[0], 'H'))
+            for i in word[1:]:
+                res.append((i, 'T'))
+            return res
 
-def head_tail_single_tagger(word):
-    if len(word) == 1:
-        return [(word, 'S')]
-    elif len(word) > 1:
-        return [(word[0], 'H')] + [(i, 'T') for i in word[1:]]
-    else:
-        return []
+        TagSet.__init__(self, ['H'], ['T'], head_tail_tagger)
 
-head_tail_single = TagSet(['H', 'S'], ['T'], head_tail_single_tagger)
+
+class HeadTailSingleTagSet(TagSet):
+    """A tag set distinguishes only whether the character is the head of the word, or itself alone is a word.
+    """
+
+    def __init__(self):
+        def head_tail_single_tagger(word):
+            if len(word) == 1:
+                return [(word, 'S')]
+            elif len(word) > 1:
+                return [(word[0], 'H')] + [(i, 'T') for i in word[1:]]
+            else:
+                return []
+
+        TagSet.__init__(self, ['H', 'S'], ['T'], head_tail_single_tagger)
 
 def demo():
+    head_tail = HeadTailTagSet()
+    head_tail_single = HeadTailSingleTagSet()
     sent = ['ABC', 'BAC', 'A', 'B']
     print sent
     print [i for i in head_tail.tag(sent)]
