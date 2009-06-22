b789cfce49aa9121d1154dc887d552abd5484838
ngu.kho <ngu.kho@0dda53ea-3736-11de-8fef-1f5ae417a2df>
2009-06-22 17:18:18 +0000

    Add char_class func

--- trunk/entity.py
+++ trunk/entity.py
@@ -16,3 +16,18 @@
 num_list = set([i for i in u"0123456789０１２３４５６７８９零一二三四五六七八九十百千万亿壹贰叁肆伍陆柒捌玖拾○佰仟.%％"])
 date_list = set([i for i in u"年月日周时分秒"])
 letter_list = set([i for i in u"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ"])
+
+C_NUM = 0
+C_DATE = 1
+C_LETTER = 2
+C_OTHER = 3
+
+def char_class(char):
+    if char in num_list:
+        return C_NUM
+    elif char in date_list:
+        return C_DATE
+    elif char in letter_list:
+        return C_LETTER
+    else:
+        return C_OTHER
