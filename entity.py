a11a23b981c7218001cc50b6e97d8259aa61ea13
ngu.kho <ngu.kho@0dda53ea-3736-11de-8fef-1f5ae417a2df>
2009-06-22 06:48:34 +0000

    missed an i

--- entity.py
+++ entity.py
@@ -15,4 +15,4 @@
 # Prepare lists for character classification
 num_list = set([i for i in u"0123456789０１２３４５６７８９零一二三四五六七八九十百千万亿壹贰叁肆伍陆柒捌玖拾○佰仟.%％"])
 date_list = set([i for i in u"年月日周时分秒"])
-letter_list = set([i for in u"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ"])
+letter_list = set([i for i in u"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ"])
